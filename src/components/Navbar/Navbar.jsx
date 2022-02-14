7e2ff341083bd84b8ff7dbd56c7daad06f796a95
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-02-14 11:52:29 +0100

    navbar markup mostly done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { StyledDiv } from './Navbar.style';
 import { images } from '../../images/images';
+import sublinks from '../../data';
+import { FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
     <StyledDiv>
-      <nav className='innner-nav'>
-        <img src={images.hero} alt='' />
+      <nav className='inner-nav'>
+        <img src={images.logo} alt='' />
+        <ul className='menu-items'>
+          {sublinks.map((item, index) => {
+            return (
+              <li key={index}>
+                <a href='#'>{item.page}</a>
+              </li>
+            );
+          })}
+        </ul>
+        <button className='sign-in-btn'>Sign in</button>
+        <button className='sidebar-btn'>
+          <FaBars />
+        </button>
       </nav>
     </StyledDiv>
   );
--- src/components/Navbar/Navbar.style.jsx
+++ src/components/Navbar/Navbar.style.jsx
@@ -1,5 +1,62 @@
 import styled from 'styled-components';
 
 export const StyledDiv = styled.div`
-  /* background: red; */
+  .inner-nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 0;
+    width: 95%;
+    max-width: 1500px;
+    margin: auto;
+    img {
+      width: 100%;
+      max-width: 130px;
+    }
+    .menu-items {
+      display: none;
+    }
+    .sign-in-btn {
+      display: none;
+    }
+    .sidebar-btn {
+      display: flex;
+      background: #000;
+      color: white;
+      border: none;
+      padding: 0.4rem;
+      border-radius: 5px;
+      cursor: pointer;
+      font-size: 1.3rem;
+    }
+  }
+
+  /* MEDIA */
+  @media (min-width: 768px) {
+    .inner-nav {
+      .menu-items {
+        display: flex;
+        align-items: center;
+        li {
+          a {
+            display: inline-block;
+            padding: 1rem;
+            color: #000;
+          }
+        }
+      }
+      .sign-in-btn {
+        display: inline;
+        background: #000;
+        color: white;
+        border: none;
+        padding: 0.5rem;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      .sidebar-btn {
+        display: none;
+      }
+    }
+  }
 `;
