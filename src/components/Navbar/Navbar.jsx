73a8d5779c55914c57d564a892d1ba8c363a6493
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-03-10 18:00:47 +0100

    working on submenues, need to put isSubmenuOpen hook in a context in order to get onMouseLeave to listen to anything outsided the button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyledDiv } from './Navbar.style';
 import { images } from '../../images/images';
 import sublinks from '../../data';
 import { FaBars } from 'react-icons/fa';
 import { useGlobalContext } from '../../context';
+// import { handleSubmenu } from './Navbar.logic';
+// import { useNavbarLogic } from './Navbar.logic';
 
 const Navbar = () => {
   const { handleSidebar } = useGlobalContext();
+
   return (
     <StyledDiv>
       <nav className='inner-nav'>
@@ -14,9 +17,12 @@ const Navbar = () => {
         <ul className='menu-items'>
           {sublinks.map((item, index) => {
             return (
-              <li key={index}>
-                <button className='menu-btn'>{item.page}</button>
-              </li>
+              <MenuButtons
+                key={index}
+                item={item}
+                // isSubmenuOpen={isSubmenuOpen}
+                // setIsSubmenuOpen={setIsSubmenuOpen}
+              />
             );
           })}
         </ul>
@@ -29,4 +35,31 @@ const Navbar = () => {
   );
 };
 
+const MenuButtons = ({ item }) => {
+  // console.log(item);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+  useEffect(() => {
+    console.log(isSubmenuOpen);
+  }, [isSubmenuOpen]);
+
+  return (
+    <>
+      <li>
+        <button
+          className='menu-btn'
+          onMouseEnter={() => setIsSubmenuOpen(true)}
+          // onMouseLeave={() => setIsSubmenuOpen(false)}
+        >
+          {item.page}
+        </button>
+        {isSubmenuOpen && <Submenu />}
+      </li>
+    </>
+  );
+};
+
+const Submenu = () => {
+  return <h1>zsdgfsdfg</h1>;
+};
 export default Navbar;
