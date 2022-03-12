44dbbce3d76187aa860bf51ae713c50a30ffd559
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-03-12 11:57:49 +0100

    changed icons and deleted unsused logic files

--- src/data.js
+++ src/data.js
@@ -1,28 +1,44 @@
-import { FaCreditCard, FaBook, FaBriefcase } from 'react-icons/fa';
+import { FaCreditCard, FaBook, FaBriefcase, FaTerminal } from 'react-icons/fa';
+import {
+  RiContactsLine,
+  RiBillLine,
+  RiCustomerService2Line,
+} from 'react-icons/ri';
+import { BsPlugFill } from 'react-icons/bs';
+import { BiHelpCircle } from 'react-icons/bi';
+import { HiOutlineInformationCircle } from 'react-icons/hi';
 import React from 'react';
 const sublinks = [
   {
     page: 'products',
     links: [
       { label: 'payment', icon: <FaCreditCard />, url: '/products' },
-      { label: 'terminal', icon: <FaCreditCard />, url: '/products' },
-      { label: 'connect', icon: <FaCreditCard />, url: '/products' },
+      { label: 'terminal', icon: <FaTerminal />, url: '/products' },
+      { label: 'connect', icon: <RiContactsLine />, url: '/products' },
     ],
   },
   {
     page: 'developers',
     links: [
-      { label: 'plugins', icon: <FaBook />, url: '/products' },
+      { label: 'plugins', icon: <BsPlugFill />, url: '/products' },
       { label: 'libraries', icon: <FaBook />, url: '/products' },
-      { label: 'help', icon: <FaBook />, url: '/products' },
-      { label: 'billing', icon: <FaBook />, url: '/products' },
+      { label: 'help', icon: <BiHelpCircle />, url: '/products' },
+      { label: 'billing', icon: <RiBillLine />, url: '/products' },
     ],
   },
   {
     page: 'company',
     links: [
-      { label: 'about', icon: <FaBriefcase />, url: '/products' },
-      { label: 'customers', icon: <FaBriefcase />, url: '/products' },
+      {
+        label: 'about',
+        icon: <HiOutlineInformationCircle />,
+        url: '/products',
+      },
+      {
+        label: 'customers',
+        icon: <RiCustomerService2Line />,
+        url: '/products',
+      },
     ],
   },
 ];
